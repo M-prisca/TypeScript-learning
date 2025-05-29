44fad472bae51147aaede3604f405f43485b4cda
prisca <priscamasereli@gmail.com>
2025-05-29 09:23:33 +0200

    feat: Mini task 2_Number types

---
+++ Mini-task2_Number types/index.js
@@ -0,0 +1,32 @@
+const reviewTotalDisplay = document.querySelector('#reviews')
+
+const reviews = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
+
+// Solution 
+
+//function showReviewTotal (value: number) { for typescript
+function showReviewTotal (value) {
+    reviewTotalDisplay.innerHTML = 'Review total :' + value.toString()
+}
+
+showReviewTotal(reviews.length)
+ 
