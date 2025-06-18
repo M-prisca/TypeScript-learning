6b403f24e78610c5c197b15417da30e2ddf5da9f
prisca <priscamasereli@gmail.com>
2025-06-18 08:25:43 +0200

    feat: Type Assertions-Mosh course

---
+++ src/Mosh course/Type Assertions.ts
@@ -0,0 +1,6 @@
+//  let phone = document.getElementById('phone') as HTMLInputElement
+let phone = <HTMLInputElement>document.getElementById('phone')
+ phone.value
+
+ // Type assertion is basically used to specify the target  element or type
+ // "as HTMLInputElement" specify the target element or type phone is..
